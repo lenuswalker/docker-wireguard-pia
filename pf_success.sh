835c7f4a59b2676b0d0f07ca77cfdd73a30bc277
Lenus Walker <walker.lenus@gmail.com>
2022-07-25 02:11:29 -0400

    Update pf_success.sh
    remove unnecessary additions

--- pf_success.sh
+++ pf_success.sh
@@ -21,10 +21,5 @@ if [ -n "$PF_DEST_IP" ] && [ -n "$FWD_IFACE" ]; then
   echo "$(date): Forwarding incoming VPN traffic on port $1 to $PF_DEST_IP:$1"
 fi
 
-# iptables routing
-iptables -F
-iptables -t nat -A POSTROUTING -o tap+ -j MASQUERADE
-iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
-
 # Run another user-defined script if defined and present
 [ -n "$PORT_SCRIPT" ] && [ -x "$PORT_SCRIPT" ] && echo "$(date): Running user-defined script: $PORT_SCRIPT" && eval "$PORT_SCRIPT $1" &
